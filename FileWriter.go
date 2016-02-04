705562185fed31c0e8b263ccb637a913063f0a4e
Julian Picht <julian.picht@gmail.com>
2016-02-04 10:59:20 +0100

    FileWriter: implement locking to prevent interleaved lines

--- FileWriter.go
+++ FileWriter.go
@@ -2,12 +2,14 @@ package logger
 
 import (
 	"io"
+	"sync"
 )
 
 type FileWriter struct {
 	writer    io.Writer
 	encoder   Encoder
 	separator []byte
+	lock      sync.Mutex
 }
 
 type separators struct {
@@ -30,10 +32,13 @@ func NewFileWriter(out io.Writer, e Encoder, separator []byte) *FileWriter {
 		writer:    out,
 		encoder:   e,
 		separator: separator,
+		lock:      sync.Mutex{},
 	}
 }
 
 func (f *FileWriter) Write(m Message) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
 	f.writer.Write(f.encoder.Encode(m))
 	f.writer.Write(f.separator)
 }
