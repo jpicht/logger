6c8aab9fbea7fca8537bd229e38d610934bdc2d8
Julian Picht <julian.picht@gmail.com>
2016-02-04 12:12:52 +0100

    fix typo in example

--- logger_test.go
+++ logger_test.go
@@ -15,10 +15,11 @@ func Example_main() {
 
 func ExampleLogger_Context(ctx context.Context) {
 	logger := NewStdoutLogger()
-	logger.WithData("requestId", "unique-id-from-loadbalancer").Context(ctx)
+	withLogger := logger.WithData("requestId", "unique-id-from-loadbalancer").Context(ctx)
+	_ = withLogger
 }
 
-func ExampleMustFomContext(ctx context.Context) {
+func ExampleMustFromContext(ctx context.Context) {
 	// will panic without logger
 	logger := MustFromContext(ctx)
 	logger.Trace("Found a logger")
